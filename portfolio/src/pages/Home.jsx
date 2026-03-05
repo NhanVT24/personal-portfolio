e2101ebf12cceee2b9d93d90e7deaf2bc652559a
NhanVT24 <nhan18072020@gmail.com>
2026-03-05 10:07:15 +0700

    feat: update index.css

---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+export const Home = () => {
+    return <div>
+
+        {/*Theme Toggle*/}
+
+        {/*Background Animation*/}
+
+        {/* Navbar*/}
+        
+        {/*Main Section*/}
+
+        {/*Footer*/}
+    </div>
+}
